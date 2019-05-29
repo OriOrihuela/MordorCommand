4a30b8bc22dfa1003e11e33b8b9bd0e726666f11
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-05-29 22:45:58 +0200

    printarStatus() done to pass test_printar_status.

--- Project/src/main/java/org/lasencinas/commandpattern/procesador/tiposProcesador/Oficina.java
+++ Project/src/main/java/org/lasencinas/commandpattern/procesador/tiposProcesador/Oficina.java
@@ -1,6 +1,8 @@
 package org.lasencinas.commandpattern.procesador.tiposProcesador;
 
+import org.lasencinas.commandpattern.pedido.interfaces.Pedido;
 import org.lasencinas.commandpattern.pedido.interfaces.TratamientoPedido;
+import org.lasencinas.commandpattern.procesador.enums.Status;
 import org.lasencinas.commandpattern.procesador.interfaces.Procesador;
 
 public class Oficina implements Procesador {
@@ -18,5 +20,15 @@ public class Oficina implements Procesador {
             return true;
         }
         return false;
+    }
+
+    public String printarStatus(boolean processable, Pedido pedido) {
+        String stringToReturn = "";
+        if (processable) {
+            stringToReturn = pedido.destino() + " " + Status.ACEPTADO;
+            return stringToReturn;
+        }
+        stringToReturn = pedido.destino() + " " + Status.RECHAZADO;
+        return stringToReturn;
     }
 }
